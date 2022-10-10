5e49fd5c690074d61c0d141f5bcb04aed8bcfbfe
pedramsaharkhiz <pedram_saharkhiz@yahoo.com>
2022-10-10 12:53:05 +0330

    end of section15

---
+++ client/src/app/_services/message.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { getPaginatedResult, getPaginationHeaders } from './paginatiionHelper';
+import { Message } from '../_models/message';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+  baseUrl="https://localhost:5001/api/";
+
+  constructor(private http:HttpClient) { }
+  getMessages(pageNumber,pageSize,container){
+    let params=getPaginationHeaders(pageNumber,pageSize);
+    params=params.append('Container',container);
+    return getPaginatedResult<Message[]>(this.baseUrl+'messages',params,this.http);
+  }
+  getMessageThread(username:string){
+    return this.http.get<Message[]>(this.baseUrl+'messages/thread/'+username);
+  }
+  sendMessage(username:string,content:string){
+    return this.http.post<Message>(this.baseUrl+'messages',{recipientUsername:username,content})
+  }
+  deleteMessage(id:number){
+    return this.http.delete(this.baseUrl+'messages/'+id);
+  }
+}
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -1,4 +1,7 @@
+import { Message } from './../_models/message';
 import { Component, OnInit } from '@angular/core';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
 
 @Component({
   selector: 'app-messages',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./messages.component.css']
 })
 export class MessagesComponent implements OnInit {
-
-  constructor() { }
+  messages:Message[]=[];
+  pagination:Pagination;
+  container='Unread';
+  pageNumber=1;
+  pageSize=5;
+  loading=false;
+  constructor(private messageService:MessageService) { }
 
   ngOnInit(): void {
+    this.loadMessages();
+  }
+  loadMessages(){
+    this.loading=true;
+    this.messageService.getMessages(this.pageNumber,this.pageSize,this.container).subscribe(
+      response=>{
+        this.messages=response.result;
+        this.pagination=response.pagination;
+        this.loading=false;
+      }
+    )
+  }
+  deleteMessage(id:number){
+    this.messageService.deleteMessage(id).subscribe(()=>{
+      this.messages.splice(this.messages.findIndex(m=>m.id===id),1);
+    })
+  }
+  pageChanged(event:any){
+    this.pageNumber=event.page;
+    this.loadMessages();
   }
 
 }
